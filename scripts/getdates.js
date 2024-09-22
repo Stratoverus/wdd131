b1ad35d152568349c4b7e630a4b6c5de7dade3a3
stratoverus <keitheberhard93@gmail.com>
2024-09-21 21:30:09 -0700

    js updates

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+const lastModified = document.lastModified();
+const currentYear = today.getFullYear();
+
+
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
