1c9da46171a41c42347bc7504bfcd0bb2014ea8e
stratoverus <keitheberhard93@gmail.com>
2024-10-03 23:42:06 -0700

    fixed some css

--- scripts/place.js
+++ scripts/place.js
@@ -8,8 +8,21 @@ window.onload = function() {
     document.getElementById('lastModified').textContent = lastModified;
 };
 
-let temperature = 87;
+let temperature = 84;
 let windSpeed = 3;
 
 document.getElementById('temperature').textContent = temperature;
 document.getElementById('wind').textContent = windSpeed;
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 50 && windSpeed > 3) {
+        let windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+        return Math.round(windChill);
+    } else {
+        return "N/A";
+    }
+}
+
+let windChill = calculateWindChill(temperature,windSpeed);
+document.getElementById("windChill").innerText = windChill;
+document.getElementById("windChillUnit").innerText = (windChill !== "N/A") ? "°F" : "";
