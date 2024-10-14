81e1404c6b60e3a4ff17f8fd5821bdba1e9a950f
stratoverus <keitheberhard93@gmail.com>
2024-10-13 21:35:40 -0700

    updating temple pics

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -21,21 +21,21 @@ const temples = [
         location: "American Fork, Utah, United States",
         dedicated: "1996, October, 19",
         area: 107240,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/mount-timpanogos-utah-temple/mount-timpanogos-utah-temple-36979.jpg"
+        imageUrl: "images/mount-timpanogos-temple.jpg"
     },
     {
         templeName: "Mesa Arizona",
         location: "Mesa, Arizona, United States",
         dedicated: "1927, October, 26",
         area: 75000,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/mesa-arizona-temple/mesa-arizona-temple-46561-main.jpg"
+        imageUrl: "images/mesa-arizona-temple.jpeg"
     },
     {
         templeName: "Gilbert Arizona",
         location: "Gilbert, Arizona, United States",
         dedicated: "2014, March, 2",
         area: 85326,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/gilbert-arizona-temple/gilbert-arizona-temple-3801.jpg"
+        imageUrl: "images/gilbert-arizona.jpg"
     },
     {
         templeName: "Aba Nigeria",
@@ -43,7 +43,7 @@ const temples = [
         dedicated: "2005, August, 7",
         area: 11500,
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        "images/aba-nigeria-temple-lds.jpg"
       },
       {
         templeName: "Manti Utah",
@@ -51,7 +51,7 @@ const temples = [
         dedicated: "1888, May, 21",
         area: 74792,
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        "images/manti-utah-lds.jpg"
       },
       {
         templeName: "Payson Utah",
@@ -59,7 +59,7 @@ const temples = [
         dedicated: "2015, June, 7",
         area: 96630,
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        "images/payson-utah-temple-daylight.jpg"
       },
       {
         templeName: "Yigo Guam",
@@ -67,7 +67,7 @@ const temples = [
         dedicated: "2020, May, 2",
         area: 6861,
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        "images/yigo-guam-temple.jpg"
       },
       {
         templeName: "Washington D.C.",
@@ -75,7 +75,7 @@ const temples = [
         dedicated: "1974, November, 19",
         area: 156558,
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        "images/washington-dc-temple.jpeg"
       },
       {
         templeName: "Lima Perú",
@@ -83,7 +83,7 @@ const temples = [
         dedicated: "1986, January, 10",
         area: 9600,
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        "images/lima-peru-temple.jpg"
       },
       {
         templeName: "Mexico City Mexico",
@@ -91,7 +91,7 @@ const temples = [
         dedicated: "1983, December, 2",
         area: 116642,
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        "images/mexico-city-temple.jpg"
       },
       {
         templeName: "London England",
@@ -99,7 +99,7 @@ const temples = [
         dedicated: "1958, September, 9",
         area: 42652,
         imageUrl:
-        "https://churchofjesuschristtemples.org/assets/img/temples/london-england-temple/london-england-temple-53656-thumb.jpg"
+        "images/london-england-temple.jpg"
       },
 ]
 
