c6555f89a3a3ffa8ddfb89d0261b5588ac6c29c2
stratoverus <keitheberhard93@gmail.com>
2024-10-26 14:47:01 -0700

    more pics

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -7,3 +7,10 @@ window.onload = function() {
     const lastModified = document.lastModified;
     document.getElementById('lastModified').textContent = lastModified;
 };
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
